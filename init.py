9ad701f7e24c4adb1acf56ddb33c28d49c17c0f6
The10thHunter <prashant9mehta.pm@gmail.com>
2022-06-27 17:34:04 -0700

    Saving...

---
+++ init.py
@@ -0,0 +1,11 @@
+#Initilizes data-compiler and provides user query to application
+
+team_num = input("Team Number Search:")
+
+user_req = input("Raw or Compiled? (Data)")
+user_req = str.lower(user_req)
+
+if user_req = compiled
+    #import score_avg, ect.
+    #print("score_avg, ect.") (but prettier) 
+
