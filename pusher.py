882014fbeecbf11552aa00738bec285564c58c1e
The10thHunter <prashant9mehta.pm@gmail.com>
2022-10-05 22:10:18 -0700

    Compiler changes, unfinished, for device swap

--- pusher.py
+++ pusher.py
@@ -1,6 +1,5 @@
-import gspread
 import pandas as pd
-import sensitive, parser
+import sensitive, parser, gspread
 #Change the file path to general later, perhaps even find a way to get the pwd of the running terminal 
 gc = gspread.service_account()
 try: 
@@ -16,6 +15,7 @@ try:
     worksheet = sheet.worksheet('Raw Data')
 except: 
     worksheet = sheet.add_worksheet(title = 'Raw Data', rows = 600, cols = 30)
+    sheet.del_worksheet('Sheet 1')
 #Insert your Gmail in sensitive.py
 sheet.share(sensitive.real_email, perm_type = 'user', role = 'writer')
 worksheet.update([parser.data.columns.values.tolist()] + parser.data.values.tolist())
