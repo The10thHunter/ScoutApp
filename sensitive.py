ac0fc05e4809bbf9d20c33a05a9768268e8c76f9
The10thHunter <prashant9mehta.pm@gmail.com>
2022-11-10 12:26:13 -0800

    Adding sensitive w/ link to gsheet

---
+++ sensitive.py
@@ -0,0 +1,4 @@
+tba_key = ""
+frc_event = ""
+email = "prashant.mehta@valkyrierobotics.com"
+sheet_url = "https://docs.google.com/spreadsheets/d/1HR2U824YTY4hApZK8BNjt6oLpHGwbNPJqVupDHKWkR0/edit?usp=sharing"
